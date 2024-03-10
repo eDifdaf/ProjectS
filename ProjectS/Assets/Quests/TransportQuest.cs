4988dbf7c7f6202b378988a3c1b8dafc5027a6d4
Difdaf <95475192+eDifdaf@users.noreply.github.com>
2024-03-10 19:19:17 +0100

    final commit before merge

--- ProjectS/Assets/Quests/TransportQuest.cs
+++ ProjectS/Assets/Quests/TransportQuest.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class testQuest : QuestObjective
+public class TransportQuest : QuestObjective
 {
     private void OnTriggerEnter(Collider other) {
         CompleteObjective();
     }
-    
 }
