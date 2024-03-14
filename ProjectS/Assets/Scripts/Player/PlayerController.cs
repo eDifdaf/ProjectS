cc4f22ef907e67eaa8045469874947d6fd6e20b8
werwolfy <raphael.nigl@outlook.de>
2024-03-14 23:36:59 +0100

    Son of a B Rotate on hold

--- ProjectS/Assets/Scripts/Player/PlayerController.cs
+++ ProjectS/Assets/Scripts/Player/PlayerController.cs
@@ -1,49 +1,82 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+//[ActionMap].[Action].performed/cancled/hold += [context] => what it should do 
 public class PlayerController : MonoBehaviour
 {
     private PlayerInputActions playerInputActions;
     private InputAction drive;
     [SerializeField] private PlayerMotor playerMotor;
-
-
-
-
-
+    private bool isTurningLeft, isTurningRight;
+    
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
-        //Get your GameKomponents
         
-
-
-        //set your Evets
-
-        //algemein wenn ich mal was erklären muss => 
-        //[ActionMap].[Action].performed/cancled/hold += [context] => what it should do 
-
-
-
     }
+
+    private void Start()
+    {
+        playerInputActions.ByBike.TurnaroundLeftStart.performed += ctx => TurningLeftPressed();//nene Error
+        playerInputActions.ByBike.TurnaroundLeftEnd.performed += ctx => TurningLeftReleased();
+        
+        playerInputActions.ByBike.TurnaroundRightStart.performed += ctx => TurningRightPressed();
+        playerInputActions.ByBike.TurnaroundRightEnd.performed += ctx => TurningRightReleased();
+    }
+
 
     private void FixedUpdate()
     {
         //Movement
         playerMotor.Moving(drive.ReadValue<Vector2>());
+        if (isTurningLeft)
+        {
+            
+            Debug.Log("ee");
+            playerMotor.RotatingLeft();
+        }
+        if (isTurningRight)
+            playerMotor.RotatingRight();
 
     }
 
+    #region EnableAndDisable
     private void OnEnable()
     {
         drive = playerInputActions.ByBike.Drive;
         drive.Enable();
+
+        
     }
+    
 
     private void OnDisable()
     {
         drive.Disable();
+
     }
+    #endregion
+
+    private void TurningLeftPressed()
+    {
+        Debug.Log("Truee");
+        isTurningLeft = true;
+    }
+    private void TurningLeftReleased()
+    {
+        isTurningLeft = false;
+    }
+    private void TurningRightPressed()
+    {
+        isTurningRight = true;
+    }
+    private void TurningRightReleased()
+    {
+        isTurningRight = false;
+    }
+
+    
 
 
 }
--- ProjectS/Assets/Scripts/Player/PlayerMotor.cs
+++ ProjectS/Assets/Scripts/Player/PlayerMotor.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
+    [SerializeField] private GameObject left;
+    [SerializeField] private GameObject right;
     private Vector3 curentVelocity;
     public float speed = 5f;
     [SerializeField] private float gravity = 9.81f;
@@ -17,8 +19,18 @@ public class PlayerMotor : MonoBehaviour
         controller.Move(transform.TransformDirection(moveInDirection * speed) * Time.deltaTime);
     }
 
+    public void RotatingLeft()
+    {
+        transform.RotateAround(left.transform.position, Vector3.up, -20 * Time.deltaTime);
+    }
+    public void RotatingRight()
+    {
+        transform.RotateAround(right.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+
     private void FixedUpdate()
     {
         controller.Move(Vector3.down * (gravity * Time.deltaTime));
+        
     }
 }
