ff86b9c3269b37eac131843dde2b6a56217ad5b8
Raphael <R.Nigl@htlkrems.at>
2024-05-28 08:55:28 +0200

    Sender and Colectables

---
+++ ProjectS/Assets/Scripts/Collectable/Sender.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sender : MonoBehaviour
+{
+    private bool isPlayerInRange;
+    [SerializeField] private GameObject player;
+
+
+    
+    
+    public void PlayerInteract()
+    {
+        if (isPlayerInRange && GameManager.Instance.DialogManager.IsEnabled == false) {
+            
+            //Code the sender should do
+            
+            //this is a placeholder
+            player.GetComponent<Currency>().SetCurrencyCount(10);
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player entered the trigger");
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        isPlayerInRange = true;
+
+
+        
+        player.GetComponent<PlayerController>().sender = this;
+
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        isPlayerInRange = false;
+
+
+        player.GetComponent<PlayerController>().sender = null;
+    }
+
+}
