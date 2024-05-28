66666713b171c7f5412da899ef5c616d8cd2b12c
Raphael <R.Nigl@htlkrems.at>
2024-05-28 13:33:11 +0200

    new Movement Wheel Collider

--- ProjectS/Assets/Scripts/Player/PlayerMotor.cs
+++ ProjectS/Assets/Scripts/Player/PlayerMotor.cs
@@ -1,37 +1,49 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMotor : MonoBehaviour
 {
-    [SerializeField] private CharacterController controller;
-    [SerializeField] private GameObject left;
-    [SerializeField] private GameObject right;
-    private Vector3 curentVelocity;
-    public float speed = 5f;
-    public float turnSpeed = 20f;
-    [SerializeField] private float gravity = 9.81f;
+    // maxPower values
+    [SerializeField] private float maxForcePower, maxAngle;
+    // Input values -1 to 1
+    [SerializeField] public float currentAngle;
+    [SerializeField] public float forcePower;
+    [SerializeField] private WheelCollider frontWheelCollider, backWheelCollider;
+    [SerializeField] private Transform frontWheel, backWheel;
+    [SerializeField] public Rigidbody bikeRb;
+    [SerializeField] private float leanAngle;
+    [SerializeField] public GameObject bike;
 
-
-
-    public void Moving(Vector2 input)
-    {
-        Vector3 moveInDirection = Vector3.zero;
-        moveInDirection.x = input.x;
-        moveInDirection.z = input.y;
-        controller.Move(transform.TransformDirection(moveInDirection * speed) * Time.deltaTime);
-    }
-
-    public void RotatingLeft()
-    {
-        transform.RotateAround(left.transform.position, Vector3.up, -turnSpeed * Time.deltaTime);
-    }
-    public void RotatingRight()
-    {
-        transform.RotateAround(right.transform.position, Vector3.up, turnSpeed * Time.deltaTime);
-    }
-
+    
+    
     private void FixedUpdate()
     {
-        controller.Move(Vector3.down * (gravity * Time.deltaTime));
-        
+        Drive();
+        Steer();
     }
+    
+    public void Drive()
+    {
+        backWheelCollider.motorTorque = maxForcePower * forcePower;
+    }
+    
+    public void Steer()
+    {
+        frontWheelCollider.steerAngle = maxAngle * currentAngle;
+
+        /*bike.transform.eulerAngles.y = maxAngle * currentAngle;*/
+    }
+    
+    //todo
+    /*private void ApplyForceToBikeRigidbody()
+    {
+        bikeRb.velocity =
+    } */
+    
+
+    
+
+
+
 }
